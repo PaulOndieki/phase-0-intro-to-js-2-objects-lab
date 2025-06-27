b3106d855842127315f25d25c47e1486aa66345d
PaulOndieki <ondiekipaulo.17@gmail.com>
2025-06-27 10:30:03 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution in this file!
+// Define the employee object in global scope
+const employee = {
+  name: "Sam",
+  streetAddress: "11 Broadway"
+};
+
+// 1. Non-destructively update employee with a key/value pair
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value
+  };
+}
+
+// 2. Destructively update employee with a key/value pair
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+// 3. Non-destructively delete a key from employee
+function deleteFromEmployeeByKey(employee, key) {
+  const newEmployee = { ...employee };
+  delete newEmployee[key];
+  return newEmployee;
+}
+
+// 4. Destructively delete a key from employee
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
+
+// Export all functions for tests
+module.exports = {
+  employee,
+  updateEmployeeWithKeyAndValue,
+  destructivelyUpdateEmployeeWithKeyAndValue,
+  deleteFromEmployeeByKey,
+  destructivelyDeleteFromEmployeeByKey
+};
